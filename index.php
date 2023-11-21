ef20065d6480b354430e6a475166817e6d8582b3
slozohancky <il.slob93@gmail.com>
2023-11-21 10:40:07 +0200

    init ht-2, RGB, create properties and methods for them

--- index.php
+++ index.php
@@ -1,21 +1,60 @@
 <?php
-phpinfo();
-?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>docker</title>
-</head>
-<body>
+class ValueObject {
+    private int $red;
+    private int $green;
+    private int $blue;
 
-<h1>Hello world</h1>
-<h1>Hello world</h1>
-<h1>Hello world</h1>
+    public function getRed(): int
+    {
+        return $this->red;
+    }
 
-</body>
-</html>
+    public function setRed(int $red): void
+    {
+        if(!($red < 0 || $red > 255)){
+            $this->red = $red;
+        }else{
+            echo "this is not a valid colour";
+        }
+    }
+
+    public function getGreen(): int
+    {
+        return $this->green;
+    }
+
+    public function setGreen(int $green): void
+    {
+        if(!($green < 0 || $green > 255)){
+            $this->green = $green;
+        }else{
+            echo "this is not a valid colour";
+        }
+    }
+
+    public function getBlue(): int
+    {
+        return $this->blue;
+    }
+
+    public function setBlue(int $blue): void
+    {
+        if($blue < 0 || $blue > 255){
+            echo "this is not a valid colour";
+        }else{
+            $this->blue = $blue;
+        }
+    }
+}
+
+$color_1 = new ValueObject();
+
+$color_1->setRed(12);
+$color_1->setGreen(0);
+$color_1->setBlue(10);
+
+echo $color_1->getRed();
+echo $color_1->getGreen();
+echo $color_1->getBlue();
+
