25249cbaf354d10ce333db9dbf834b47b98e211a
slozohancky <il.slob93@gmail.com>
2023-11-26 20:16:08 +0200

    i did ht-8 Dependency inversion

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
 
-    require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-    d("docker");
+interface iConnected{
+    public function getData();
+}
 
+class Mysql implements iConnected
+{
+    public function getData()
+    {
+        return 'some data from database';
+    }
+}
+
+class Controller
+{
+    private $adapter;
+
+    public function __construct(iConnected $mysql)
+    {
+        $this->adapter = $mysql;
+    }
+
+    function getData()
+    {
+        $this->adapter->getData();
+    }
+}
+
+$mysql = new Mysql();
+
+$controller = new Controller($mysql);
+
+d($controller);
